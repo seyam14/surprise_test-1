7695bc28224fa2c58ce29f9b31f81cda15250f4e
seyam <mdseyam3215@gmail.com>
2023-11-29 21:19:20 +0600

    work complete

--- lib/main.dart
+++ lib/main.dart
@@ -8,49 +8,67 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/',
-      routes: {
-        '/': (context) => LoginPage(),
-        '/search': (context) => SearchPage(),
-      },
+      home: LoginPage(),
     );
   }
 }
 
-class  LoginPage extends StatelessWidget {
+class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Login Page'),
+        title:const Text('Login Page'),
       ),
-      backgroundColor: Colors.purple,
-      body: Center(
+      body: Padding(
+        padding:
+        const EdgeInsets.only(left: 50, right: 50, top: 10, bottom: 10),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            const Text(
-              'Welcome ',
-              style: TextStyle(
-                fontWeight: FontWeight.bold, // Make text bold
-                fontSize: 35.0, // Set font size to 20.0
+            const SizedBox(height: 15),
+           const SizedBox(height: 25),
+            const Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Welcome',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 48,
+                ),
               ),
             ),
+           const SizedBox(height: 30),
             const TextField(
-              textAlign: TextAlign.center, // Center-align the text field
-              decoration: InputDecoration(labelText: 'Username'),
+              decoration: InputDecoration(
+                hintText: 'Username',
+              ),
             ),
-            const TextField(
-              textAlign: TextAlign.center, // Center-align the text field
-              decoration: InputDecoration(labelText: 'Password'),
+           const SizedBox(height: 30),
+           const TextField(
               obscureText: true,
+              decoration: InputDecoration(
+                hintText: 'Password',
+              ),
             ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/search');
-              },
-              child: const Text('Login'),
+           const SizedBox(height: 30),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SearchPage()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.purple,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                child: const Text('Login'),
+              ),
             ),
           ],
         ),
@@ -59,103 +77,316 @@ class  LoginPage extends StatelessWidget {
   }
 }
 
-
 class SearchPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Search Page'),
+        title:const Text('Search Page'),
       ),
-      backgroundColor: Colors.purple,
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Hi ! there',
+                style: TextStyle(
+
+                  fontSize: 20,
+                ),
+              ),
+            ),
+
+            const Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Bus',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 48,
+                ),
+              ),
+            ),
+
+           const SizedBox(height: 20),
+            Card(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Column(
+                      children: [
+                        Row(
+                          children: [
+                            Icon(Icons.location_on, color: Colors.blue),
+                            SizedBox(width: 8),
+                            Text('Location 1'),
+                          ],
+                        ),
+                        SizedBox(height: 10),
+                        Row(
+                          children: [
+                            Icon(Icons.location_on, color: Colors.greenAccent),
+                            SizedBox(width: 8),
+                            Text('Location 2'),
+                          ],
+                        ),
+                        SizedBox(height: 10),
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: 40,
+                          height: 40,
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.blue,
+                          ),
+                          child: const Icon(
+                            Icons.arrow_upward,
+                            color: Colors.white,
+                          ),
+                        ),
+                      const SizedBox(height: 10),
+                        Container(
+                          width: 40,
+                          height: 40,
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.blue,
+                          ),
+                          child: const Icon(
+                            Icons.arrow_downward,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+
+
+          const  SizedBox(height: 20),
+            const Card(
+              child: Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: [
+                        Row(
+                          children: [
+                            SizedBox(width: 8),
+                            Text('Passenger :',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                              )
+
+                            ),
+
+                          ],
+                        ),
+                        SizedBox(height: 10),
+                        Row(
+                          children: [
+                            SizedBox(width: 8),
+                            Text('Dept :',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                )
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 10),
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+
+                        SizedBox(height: 10),
+                        Text('Type',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                            )
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+
+
+           const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => LocationPage()),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0), // Adjust the value for roundness
+                ),
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16), // Adjust padding for size
+              ),
+              child: const Text(
+                'Search',
+                style: TextStyle(fontSize: 18), // Adjust the font size
+              ),
+            )
+
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LocationPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Location Page'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('Hi, sir.'),
-            const Text(
-              'Bus ',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 30.0,
-              ),
+          const  Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Location 1'),
+                Text('Location 2'),
+              ],
             ),
-            const SizedBox(height: 20),
+           const SizedBox(height: 20),
+            Card(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(Icons.location_on, color: Colors.blue),
+                            SizedBox(width: 8),
+                            Text('Location 1'),
+                          ],
+                        ),
+                        Text(
+                          '\$30',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ],
+                    ),
+                 const  SizedBox(height: 10),
+                  const  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(Icons.location_on, color: Colors.greenAccent),
+                            SizedBox(width: 8),
+                            Text('Location 2'),
+                          ],
+                        ),
 
-            // Box 1: Location Information
-            Container(
-              padding: const EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                color: Colors.blueGrey,
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child: const Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Location 1',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18,
+                      ],
                     ),
-                  ),
-                  SizedBox(height: 8),
-                  Text(
-                    'Location 2',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18,
+                  const  SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: Text('Buy Ticket'),
+                        ),
+                      ],
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-            const SizedBox(height: 20),
+           const SizedBox(height: 20),
+            Card(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                  const  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(Icons.location_on, color: Colors.blue),
+                            SizedBox(width: 8),
+                            Text('Location 1'),
+                          ],
+                        ),
+                        Text(
+                          '\$50',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ],
+                    ),
+                   const SizedBox(height: 10),
+                const  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(Icons.location_on, color: Colors.greenAccent),
+                            SizedBox(width: 8),
+                            Text('Location 2'),
+                          ],
+                        ),
 
-            // Box 2: Passenger Details
-            Container(
-              padding: const EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                color: Colors.blueGrey,
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child:const Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18,
+                      ],
                     ),
-                    'Passenger: Seyam',
-                  ),
-                  SizedBox(height: 8),
-                  Text(
-                    'Type: Regular',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18,
+                  const SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {},
+                          child:const  Text('Buy Ticket'),
+                        ),
+                      ],
                     ),
-                  ),
-                  SizedBox(height: 8),
-                  Text(
-                    'Date of Journey: November 23, 2023',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18,
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-            const SizedBox(height: 20),
-
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/location');
-              },
-              child: const Text('Search'),
-            ),
+           const SizedBox(height: 20),
           ],
         ),
       ),
